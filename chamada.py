215ecfc9084cbc6017a22cc3c6457f7600a2f965
Thauan Mendes <43971492+ThauanMeendes@users.noreply.github.com>
2018-10-23 17:26:55 -0300

    Create chamada.py

---
+++ chamada.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+lista = [['Brasil', 'Italia',[10,9]],['Brasil','Espanha',[5,7]],['Italia','Espanha',[7,8]]]
+
+#print(lista[1][2][1])
+#print(lista)
+a1 = lista[0][2][0] 
+a2 = lista[0][2][1] 
+a3 = lista[1][2][0] 
+a4 = lista[1][2][1] 
+a5 = lista[2][2][0]
+a6 = lista[2][2][1]
+
+result = a1 + a2 + a3 + a4 + a5 + a6 
+
+print(result)
